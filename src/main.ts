56fb070b552f617e8d8d282cc8db58b02f76e82f
Czarniecki, Jacek <jacek.czarniecki@ihsmarkit.com>
2016-09-20 01:37:21 +0200

    Update statistics to include subdirectories

--- src/main.ts
+++ src/main.ts
@@ -118,6 +118,7 @@ class FolderStructure {
     private paths: string[];
     private Root: TreeNode;
     private Dict: { [key: string]: TreeNode };
+    public static TreeDictionary: { [key: string]: TreeNode }
 
     public applyChanges() {
         this.scan();
@@ -150,6 +151,8 @@ class FolderStructure {
             .concat(this.Root)
             .reduce(((dict, n) => (dict[n.path] = n) && dict), <any>{});
 
+        FolderStructure.TreeDictionary = this.Dict;
+
         while (nodes.length > 0) {
             nodes
                 .forEach(x => {
@@ -172,25 +175,28 @@ class FolderStructure {
         var $header = $table.find('tr')[0].outerHTML;
 
         Object.keys(this.Dict)
+            .filter(x => x.length > 0)
             .forEach(x => {
                 var node = this.Dict[x];
-
-                if (!node.name.trim()) {
-                    return;
-                }
-
-                var $row = $table
-                    .find(`.file[data-value="${x}/"]`)
-                    .parents('tr');
+                var $row = node.findRow();
 
                 // $row.attr('data-ns', node.path);
 
                 $row = $row.length ? $row : $(`
                     <tr class="missing-group-row">
-                        <td class="file" data-value="${node.path}/" colspan="${columns}" style="padding-left: ${10 + (node.level - 1) * 20}px">
+                        <td class="file" data-value="${node.path}/" style="padding-left: ${10 + (node.level - 1) * 20}px">
                             <i class="fa fa-angle-down" title="Expand/collapse"></i>
-                            <i class="fa fa-folder-open"></i><a>${node.name}</a></td>
-                        </tr>
+                            <i class="fa fa-folder-open"></i><a>${node.name}</a>
+                        </td>
+                        <td data-value="0" class="pic low"><div class="chart"><div class="cover-fill" style="width: 1%;"></div><div class="cover-empty" style="width:99%;"></div></div></td>
+                        <td data-value="0" class="pct">0</td>
+                        <td data-value="0" class="abs">0/0</td>
+                        <td data-value="0" class="pct">0</td>
+                        <td data-value="0" class="abs">0/0</td>
+                        <td data-value="0" class="pct">0</td>
+                        <td data-value="0" class="abs">0/0</td>
+                        <td data-value="0" class="pct">0</td>
+                        <td data-value="0" class="abs">0/0</td>
                     </tr>`);
 
                 // $row.attr('data-ns', node.path);
@@ -241,5 +247,79 @@ class FolderStructure {
 (new FolderStructure()).applyChanges();
 
 
+let sum = (arr, selector: (x:any) => number): number => arr.reduce(((acc, next) => acc + selector(next)), 0);
+
+class FolderStatsFix {
+
+    private paths: string[];
+    private Root: TreeNode;
+    private Dict: { [key: string]: TreeNode };
+
+    public applyChanges() {
+        this.updateSummaryTable();
+    }
+    
+    private statsForColumn (node: TreeNode, column: number) : { all :number, covered :number } {
+        let cells = node.findRow().find('td');
+        let [covered, all] = cells[column].innerText.split('/').map(x => parseInt(x, 10));
+
+        if (node.isLeaf()) {
+            return {all, covered};
+        }
+        else {
+            let childrenStats = node.children.map(x => this.statsForColumn(x, column));
+            let childrenStatsSum = {
+                all: sum(childrenStats, x => x.all), 
+                covered: sum(childrenStats, x => x.covered)
+            }
+            return {
+                all: all + childrenStatsSum.all, 
+                covered: covered + childrenStatsSum.covered
+            };            
+        }
+    }
+
+    private updateSummaryTable() {
+        Object.keys(FolderStructure.TreeDictionary)
+            .filter(x => x.length > 0)
+            .map(k => FolderStructure.TreeDictionary[k])
+            .filter(node => !node.isLeaf())
+            .forEach(node => {
+                let $row = node.findRow();
+                let cells = $row.find('td');
+                
+                let statements = this.statsForColumn(node, 3);
+                let branches = this.statsForColumn(node, 5);
+                let functions = this.statsForColumn(node, 7);
+                let lines = this.statsForColumn(node, 9);
+                
+                let overallResult = (statements.covered / statements.all * 100);
+                let overallRoundedResult = Math.round(overallResult);
+                $(cells[1])
+                    .attr('data-value', overallResult.toFixed(2))
+                    .find('.cover-fill').css('width', overallRoundedResult + '%').end()
+                    .find('.cover-empty').css('width', (100 - overallRoundedResult) + '%').end()
+                    .removeClass('low medium high')
+                    .addClass(overallRoundedResult >= 50 ? overallRoundedResult >= 80 ? 'high' : 'medium' : 'low');
+                
+                cells[2].innerText = statements.all === 0 ? '-' : `${Math.round(statements.covered / statements.all * 100)}%`;
+                cells[3].innerText = `${statements.covered}/${statements.all}`;
+                
+                cells[4].innerText = branches.all === 0 ? '-' : `${Math.round(branches.covered / branches.all * 100)}%`;
+                cells[5].innerText = `${branches.covered}/${branches.all}`;
+                
+                cells[6].innerText = functions.all === 0 ? '-' : `${Math.round(functions.covered / functions.all * 100)}%`;
+                cells[7].innerText = `${functions.covered}/${functions.all}`;
+                
+                cells[8].innerText = lines.all === 0 ? '-' : `${Math.round(lines.covered / lines.all * 100)}%`;
+                cells[9].innerText = `${lines.covered}/${lines.all}`;
+
+            });
+    }
+}
+
+(new FolderStatsFix()).applyChanges();
+
+// todo: load files list and stats
 // todo: replace navigation with "expand leafs" in-place
 //  .. leave only links to leafs
